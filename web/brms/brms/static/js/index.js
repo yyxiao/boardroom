c596b0855700443110ddc14c590bb61caf49b20d
xyy <xyy-JAVA@outlook.com>
2016-08-08 19:06:22 +0800

    update: 添加局部刷新

---
+++ web/brms/brms/static/js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by cuizc on 16-8-8.
+ */
+var loadcontent = function(url) {
+	$('#mainContent').load(url,function(response,status,xhr){
+		if (status == 'error'){
+				redirect_to("/");
+		}
+	});
+}
