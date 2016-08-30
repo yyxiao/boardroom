6ec26c6bcd97efbe4ae99a2b04ebfa9bd7099ffb
xyy <xyy-JAVA@outlook.com>
2016-08-30 17:01:14 +0800

    update: 修改初始化脚本，添加测试用户

--- web/brms/brms/scripts/brms_import.sql
+++ web/brms/brms/scripts/brms_import.sql
@@ -1,6 +1,8 @@
 -- 用户初始化
-INSERT INTO "brms"."sys_user" ("id", "user_account", "user_pwd", "user_no", "user_name", "max_period", "email", "phone", "address", "user_type", "err_count", "unlock_time", "sex", "nation", "birthday", "position", "create_time", "create_user", "update_time", "org_id", "state") VALUES (1, 'sysadmin', 'MDAwMDAw', '', '系统管理员', 30, 'sysadmin@chyjr.com', '', '', 0, 0, now(), NULL, NULL, NULL, '系统管理员', NULL, NULL, NULL, 1, 1);
-SELECT SETVAL('brms.sys_user_id_seq', 2, false);
+INSERT INTO "brms"."sys_user" ("id", "user_account", "user_pwd", "user_no", "user_name", "max_period", "email", "phone", "address", "user_type", "err_count", "unlock_time", "sex", "nation", "birthday", "position", "create_time", "create_user", "update_time", "org_id", "state") VALUES (1, 'sysadmin', 'MDAwMDAw', '', '超级管理员', 30, 'sysadmin@chyjr.com', '', '', 0, 0, now(), NULL, NULL, NULL, '超级管理员', NULL, NULL, NULL, 1, 1);
+INSERT INTO "brms"."sys_user" ("id", "user_account", "user_pwd", "user_no", "user_name", "max_period", "email", "phone", "address", "user_type", "err_count", "unlock_time", "sex", "nation", "birthday", "position", "create_time", "create_user", "update_time", "org_id", "state") VALUES (2, 'jingyh', 'MDAwMDAw', '', 'jingyh', 30, 'sysadmin@chyjr.com', '', '', 0, 0, now(), NULL, NULL, NULL, '系统管理员', NULL, NULL, NULL, 1, 1);
+INSERT INTO "brms"."sys_user" ("id", "user_account", "user_pwd", "user_no", "user_name", "max_period", "email", "phone", "address", "user_type", "err_count", "unlock_time", "sex", "nation", "birthday", "position", "create_time", "create_user", "update_time", "org_id", "state") VALUES (3, 'wuyr', 'MDAwMDAw', '', 'wuyr', 30, 'sysadmin@chyjr.com', '', '', 0, 0, now(), NULL, NULL, NULL, '系统管理员', NULL, NULL, NULL, 1, 1);
+SELECT SETVAL('brms.sys_user_id_seq', 4, false);
 
 -- 机构初始化
 INSERT INTO "brms"."sys_org" ("id", "org_name", "org_type", "parent_id", "org_manager", "phone", "address", "org_seq", "state", "create_time", "create_user", "update_time") VALUES (1, '海银金控', NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
@@ -8,15 +10,19 @@ SELECT SETVAL('brms.sys_org_id_seq', 2, false);
 
 -- 用户机构初始化
 INSERT INTO "brms"."sys_user_org" ("user_id", "org_id", "create_time", "create_user") VALUES (1, 1, NULL, NULL);
+INSERT INTO "brms"."sys_user_org" ("user_id", "org_id", "create_time", "create_user") VALUES (2, 1, NULL, NULL);
+INSERT INTO "brms"."sys_user_org" ("user_id", "org_id", "create_time", "create_user") VALUES (3, 1, NULL, NULL);
 
 -- 角色初始化
-INSERT INTO "brms"."sys_role" ("role_id", "role_name", "role_desc", "create_time", "create_user") VALUES (1, '系统管理员', '系统管理员', NULL, NULL);
-INSERT INTO "brms"."sys_role" ("role_id", "role_name", "role_desc", "create_time", "create_user") VALUES (2, '普通管理员', '普通管理员', NULL, NULL);
+INSERT INTO "brms"."sys_role" ("role_id", "role_name", "role_desc", "create_time", "create_user") VALUES (1, '超级管理员', '超级管理员', NULL, NULL);
+INSERT INTO "brms"."sys_role" ("role_id", "role_name", "role_desc", "create_time", "create_user") VALUES (2, '系统管理员', '系统管理员', NULL, NULL);
 INSERT INTO "brms"."sys_role" ("role_id", "role_name", "role_desc", "create_time", "create_user") VALUES (3, '普通用户', '普通用户', NULL, NULL);
 SELECT SETVAL('brms.sys_role_id_seq', 4, false);
 
 -- 用户角色初始化
 INSERT INTO "brms"."sys_user_role" ("role_id", "user_id", "create_time", "create_user") VALUES (1, 1, NULL, NULL);
+INSERT INTO "brms"."sys_user_role" ("role_id", "user_id", "create_time", "create_user") VALUES (2, 2, NULL, NULL);
+INSERT INTO "brms"."sys_user_role" ("role_id", "user_id", "create_time", "create_user") VALUES (2, 3, NULL, NULL);
 
 -- 角色菜单初始化
 INSERT INTO "brms"."sys_role_menu" ("role_id", "menu_id", "create_time", "create_user") VALUES (1, 1, NULL, NULL);
